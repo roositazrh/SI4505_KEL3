44c6989d54d6879c23f64602b543d861fa63c106
bALE <91659708+IqbalInsanKurnia@users.noreply.github.com>
2024-05-14 04:53:00 +0700

    backend Filament

--- database/seeders/DummyUsersSeeder.php
+++ database/seeders/DummyUsersSeeder.php
@@ -18,18 +18,21 @@ class DummyUsersSeeder extends Seeder
                 'name' => 'admin',
                 'email' => 'admin@admin.com',
                 'role' => 'admin',
+                'no_telp' => '87882745232',
                 'password' => bcrypt('admin')
             ],
             [
                 'name' => 'user',
                 'email' => 'user@user.com',
                 'role' => 'user',
+                'no_telp' => '87882745232',
                 'password' => bcrypt('user')
             ],
             [
                 'name' => 'koperasi',
                 'email' => 'koperasi@koperasi.com',
                 'role' => 'koperasi',
+                'no_telp' => '87882745232',
                 'password' => bcrypt('koperasi')
             ],
         ];
