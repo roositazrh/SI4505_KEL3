fac630d54ac760db6837d7cc18a51bf40dc550cf
Dharu <116425923+dharuwidhyanugrah@users.noreply.github.com>
2024-06-08 23:10:13 +0700

    commit yang bau lagi

--- resources/views/artikel.blade.php
+++ resources/views/artikel.blade.php
@@ -132,45 +132,81 @@
 
   </main><!-- End #main -->
 
-  <!-- ======= Footer ======= -->
-  <footer id="footer">
+<!-- ======= Footer ======= -->
+<footer id="footer">
 
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
+<div class="footer-top">
+  <div class="container">
+    <div class="row">
 
-          <div class="col-lg-3 col-md-6 footer-contact">
-            <h3>AGRIMAP</h3>
-            <p>
-              Jalan Telekomunikasi <br>
-              Kabupaten Bandung, Jawa Barat 40257<br>
-              Indonesia <br><br>
-              <strong>Phone:</strong> 0812345678<br>
-              <strong>Email:</strong> agrimap@gatau.com<br>
-            </p>
-          </div>
+      <div class="col-lg-3 col-md-6 footer-contact">
+        <h3>AGRIMAP</h3>
+        <p>
+          Jalan Telekomunikasi <br>
+          Kabupaten Bandung, Jawa Barat 40257<br>
+          Indonesia <br><br>
+          <strong>Phone:</strong> 0812345678<br>
+          <strong>Email:</strong> agrimap@gatau.com<br>
+        </p>
+      </div>
 
-          <div class="col-lg-2 col-md-6 footer-links">
-            <h4>Useful Links</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="{{route('landing')}}">Home</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="{{route('landing')}}#portfolio">Galeri</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="{{route('artikel')}}">Artikel</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="{{route('landing')}}#services">Layanan</a></li>
-            </ul>
-          </div>
+      <div class="col-lg-2 col-md-6 footer-links">
+        <h4>Useful Links</h4>
+        <ul>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+          
+          <li><i class="bx bx-chevron-right"></i> <a href="#portfolio">Galeri</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="{{route('artikel')}}">Artikel</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#services">Layanan</a></li>
+        </ul>
+      </div>
 
-          <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Layanan Kami</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="{{route('pengaduan')}}">Lapak Pengaduan</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="{{route('landing')}}#contact">Peta Sumber daya</a></li>
-              <!-- <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li> -->
-            </ul>
-          </div>
+      <div class="col-lg-3 col-md-6 footer-links">
+        <h4>Layanan Kami</h4>
+        <ul>
 
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+          <li><i class="bx bx-chevron-right"></i> <a href="{{route('pengaduan')}}">Lapak Pengaduan</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#contact">Peta Sumber daya</a></li>
 
+          <!-- <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li> -->
+        </ul>
+      </div>
+
+      <div class="col-lg-4 col-md-6 footer-newsletter">
+        <h4>Artikel Kami</h4>
+        <p>Lihat berita terbaru dari berbagai desa di Indonesia</p>
+        <form action="" method="post">
+          <input type="email" name="email"><input type="submit" value="Log In">
+        </form>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<div class="container d-md-flex py-4">
+
+  <div class="me-md-auto text-center text-md-start">
+    <div class="copyright">
+      &copy; Copyright <strong><span>Agrimap</span></strong>. All Rights Reserved
+    </div>
+    <div class="credits">
+      <!-- All the links in the footer should remain intact. -->
+      <!-- You can delete the links only if you purchased the pro version. -->
+      <!-- Licensing information: https://bootstrapmade.com/license/ -->
+      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/bethany-free-onepage-bootstrap-theme/ -->
+      <!-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> -->
+    </div>
+  </div>
+  <div class="social-links text-center text-md-right pt-3 pt-md-0">
+    <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+    <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+    <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+    <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+    <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+  </div>
+</div>
+</footer><!-- End Footer -->
   <!-- Vendor JS Files -->
   <script src="{{asset('land-bt/assets/vendor/purecounter/purecounter_vanilla.js')}}"></script>
   <script src="{{asset('land-bt/assets/vendor/aos/aos.js')}}"></script>
